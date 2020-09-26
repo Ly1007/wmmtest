02d789054c2d2b899a5d27f628419f5ba3775b9e
qz_luyu <dzr8203@dingtalk.com>
2020-09-26 17:29:54 +0800

    1224-配置redis

--- application/index/controller/Test.php
+++ application/index/controller/Test.php
@@ -1,2 +1,15 @@
 <?php
 namespace app\index\controller;
+
+use think\facade\Cache;
+
+class Test
+{
+    public function index()
+    {
+        Cache::set('name','张三',3600);
+        $var = Cache::get('name');
+
+        echo $var;
+    }
+}
--- route/index.php
+++ route/index.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::group('V1', function () {
+    Route::rule('test', 'Test/index');  // 测试
     Route::rule('index', 'Index/index');  // 首页
     Route::rule('login', 'Account/login', 'POST');  // 登录
 })->middleware('Account');
