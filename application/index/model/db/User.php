2ddab661ad5b88ebadfd1a475169f1289ad0934c
Ly1007 <1600632584@qq.com>
2020-10-07 11:28:01 +0800

    1224-用户更改

--- application/index/model/db/User.php
+++ application/index/model/db/User.php
@@ -5,9 +5,14 @@ use think\Model;
 
 class User extends Model
 {
+    protected $table = 't_user';
+
     // 获取用户信息
     public function getListByName($name)
     {
+        $map = [
+            'a' => ['=','1'],
+        ];
 
     }
 }
