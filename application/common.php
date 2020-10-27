299f85e3742723e73714cea8cbc623214e31cdfa
qz_luyu <dzr8203@dingtalk.com>
2020-10-27 17:50:28 +0800

    上传图片示例

--- application/common.php
+++ application/common.php
@@ -58,3 +58,38 @@ if (!function_exists('upImg')) {
         }
     }
 }
+
+
+/**
+ * 上传图片示例
+ */
+function addimg($pic,$url){
+    // 获取表单上传文件 例如上传了001.jpg
+    // $file = request()->file($filename);
+    // 移动到框架应用根目录/public/uploads/ 目录下
+    // $file = request()->file($pic);
+    if (is_array($pic)) {
+
+        foreach($pic as $file){
+            // 'img' . DS . 'goodsimg'
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->validate(['size'=> 1024 * 1000 ,'ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public'. DS .$url);
+            $data[] = str_replace('\\', '/',$url . '/' . $info->getSaveName());
+        }
+
+        return implode(',',$data);
+    } elseif (is_object($pic)) {
+        $info = $pic->validate(['size'=> 1024 * 1000 ,'ext'=>'jpg,png,gif'])
+            ->move(ROOT_PATH . 'public'. DS . $url);
+        if($info){
+            //返回文件位置信息 如：20180620/42a79759f284b767dfcb2a0197904287.jpg
+            return  str_replace('\\', '/',$url . '/' . $info->getSaveName());
+
+        }else{
+            // 上传失败获取错误信息
+            return false;
+            // return $file->getError();
+        }
+        //单文件上传
+    }
+}
